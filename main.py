4d7d70cc1ca397a337b3dc3e4dad19ec18a91bb1
Netosss <92423203+Netosss@users.noreply.github.com>
2021-11-13 20:13:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,157 +1,34 @@
-from imutils.video import VideoStream
-import datetime
-import math
-import cv2.cv2 as cv2
-import numpy as np
-import argparse
-import imutils
-import time
-import smtplib
+import pyrebase
 
-# variaveis globais
-width = 0
-height = 0
-ContadorEntradas = 0
-ContadorSaidas = 0
-AreaContornoLimiteMin = 100  # Change these values according to your needs.
-ThresholdBinarizacao = 90  # Change these values according to your needs.
-OffsetLinhasRef = 60  # Change these values according to your needs.
-timeout = time.time() + 60 * 1  # 1 minute from now, the program will shutdown, for testing purposes or else.
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    firebaseConfig = {
+        'apiKey': "AIzaSyC1hNRnNx4zPS4lU9hRugfx3OlX7BD4sN8",
+        'authDomain': "smartdispenser-9043d.firebaseapp.com",
+        'databaseURL': "https://smartdispenser-9043d-default-rtdb.firebaseio.com",
+        'projectId': "smartdispenser-9043d",
+        'storageBucket': "smartdispenser-9043d.appspot.com",
+        'messagingSenderId': "1059644042677",
+        'appId': "1:1059644042677:web:fe599989918df767557dfc",
+        'measurementId': "G-1V8PC6SFH1"
+    }
 
+    firebase = pyrebase.initialize_app(firebaseConfig)
 
-# Verifying if the object is entering the monitored zone.
-def ObjectEntrace(y, y_entrace_coordinate, y_exit_coordinate):
-    dif = abs(y - y_entrace_coordinate)
-
-    if (dif <= 2) and (y < y_exit_coordinate):
-        return 1
-    else:
-        return 0
-
-
-# Verifying if the object is leaving the monitored zone.
-def ObjectExit(y, y_entrace_coordinate, y_exit_coordinate):
-    dif = abs(y - y_exit_coordinate)
-
-    if ((dif <= 2) and (y > y_entrace_coordinate)):
-        return 1
-    else:
-        return 0
-
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--picamera", type=int, default=-1,
-                help="whether or not the Raspberry Pi camera should be used")
-args = vars(ap.parse_args())
-
-camera = VideoStream(usePiCamera=args["picamera"] > 0).start()
-time.sleep(2.0)
-
-PrimeiroFrame = None
-
-# Making some frame reading before considering analysis, the reason is, some cameras may take longer to
-# get used to ambiente light conditions when they turn on, capturing consecutive frames with a lot of lighting
-# variation, so to not process these effects, consecutive captures are maybe outside the image processing giving the
-# camera some time to adapt to lighting conditions.
-
-for i in range(0, 20):
-    (grabbed, Frame) = camera.read(), camera.read()
-
-while True:
-    # Reading first Frame and determining size.
-    (grabbed, Frame) = camera.read(), camera.read()
-    height = np.size(Frame, 0)
-    width = np.size(Frame, 1)
-
-    # Converting Frame to Grey Scale and applying Blur Effect to highlight shapes.
-    FrameGray = cv2.cvtColor(Frame, cv2.COLOR_BGR2GRAY)
-    FrameGray = cv2.GaussianBlur(FrameGray, (21, 21), 0)
-
-    # A comparisson is made between two images, if the first frame is null, it's initialized.
-    if PrimeiroFrame is None:
-        PrimeiroFrame = FrameGray
+    db = firebase.database()
+    updating = False
+    while not updating:
+        updating = db.child("elements").get().val().get("done_updating")
         continue
+    parent = db.child("elements")
+    temp = parent.get().val()
+    amount = temp.get("amount")
+    users_list = []
+    for i in range(amount):
+        cur_user = "elements/element" + str(i)
+        users_list.append(db.child(cur_user).get())
+    for user in users_list:
+        print(user.val().get("quantity"))
+    db.child("elements").update({"done_updating": False})
 
-    # Absolute difference between initial frame and actual frame (Background Subtraction)
-    # Also makes the binarization of the frame and the subtracted background.
-    FrameDelta = cv2.absdiff(PrimeiroFrame, FrameGray)
-    FrameThresh = cv2.threshold(FrameDelta, ThresholdBinarizacao, 255, cv2.THRESH_BINARY)[1]
-
-    # Makes the dilatation of the binarized frame to eliminate holes, white zones inside the found shapes,
-    # this way, detected objects will be considered a black mass, also finds the shapes after dilatation.
-    FrameThresh = cv2.dilate(FrameThresh, None, iterations=2)
-    _, cnts, _ = cv2.findContours(FrameThresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    QtdeContornos = 0
-
-    # Drawing reference lines
-    CoordenadaYLinhaEntrada = (height / 2) - OffsetLinhasRef
-    CoordenadaYLinhaSaida = (height / 2) + OffsetLinhasRef
-    cv2.line(Frame, (0, CoordenadaYLinhaEntrada), (width, CoordenadaYLinhaEntrada), (255, 0, 0), 2)
-    cv2.line(Frame, (0, CoordenadaYLinhaSaida), (width, CoordenadaYLinhaSaida), (0, 0, 255), 2)
-
-    # Wiping found shapes
-    for c in cnts:
-        # Small shapes are to be ignored.
-        if cv2.contourArea(c) < AreaContornoLimiteMin:
-            continue
-        # For debugging purposes, counts the number of found shapes
-        QtdeContornos = QtdeContornos + 1
-
-        # Gets the shapes coordinates (A rectangle that involves the object), highlighting it's shape.
-        (x, y, w, h) = cv2.boundingRect(c)  # x e y: coordenadas do vertice superior esquerdo
-        # w e h: respectivamente largura e altura do retangulo
-
-        cv2.rectangle(Frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        # Determines the central point of the shape, and circles it.
-        CoordenadaXCentroContorno = (x + x + w) / 2
-        CoordenadaYCentroContorno = (y + y + h) / 2
-        PontoCentralContorno = (CoordenadaXCentroContorno, CoordenadaYCentroContorno)
-        cv2.circle(Frame, PontoCentralContorno, 1, (0, 0, 0), 5)
-
-        # Tests the intersection of centers from the shapes and the reference lines. This way, it may count
-        # which shapes crosses the reference lines.
-        if ObjectEntrace(CoordenadaYCentroContorno, CoordenadaYLinhaEntrada, CoordenadaYLinhaSaida):
-            ContadorEntradas += 1
-
-        if ObjectExit(CoordenadaYCentroContorno, CoordenadaYLinhaEntrada, CoordenadaYLinhaSaida):
-            ContadorSaidas += 1
-
-        # If needed, uncomment these lines to show framerate.
-        # cv2.imshow("Frame binarizado", FrameThresh)
-        # cv2.waitKey(1);
-        # cv2.imshow("Frame com subtracao de background", FrameDelta)
-        # cv2.waitKey(1);
-
-    print("Contornos encontrados: " + str(QtdeContornos))
-
-    # Writes on screen the number of people who enters or leaves the watched area.
-    cv2.putText(Frame, "Entradas: {}".format(str(ContadorEntradas)), (10, 50),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (250, 0, 1), 2)
-    cv2.putText(Frame, "Saidas: {}".format(str(ContadorSaidas)), (10, 70),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.imshow("Original", Frame)
-    key = cv2.waitKey(1) & 0xFF
-
-    # If you want to exit the program from a keystroke, uncomment these lines and comment the next ones,
-    # which makes the program exit by itself after a certain ammount of time.
-    # if the `q` key was pressed, break from the loop
-    # if key == ord("q"):
-    # break
-
-    test = 0
-    if test == 5 or time.time() > timeout:
-        break
-    test = test - 1
-
-# cleanup the camera and close any open windows
-
-# Program writes the count to a file.
-f = open('contagem.txt', 'w')  # File path and it's name with extension, to write to.
-f.write('ContadorEntradas = ' + repr(ContadorEntradas) + '\n')  # Variables to write
-f.write('ContadorSaidas = ' + repr(ContadorSaidas) + '\n')  # Variables to write
-f.close()
-
-#cv2.destroyAllWindows()
-camera.stop()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
