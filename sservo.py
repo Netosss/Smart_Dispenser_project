b7716068c0b0b3120f8b5e03ce1079830925dcd9
Netosss <92423203+Netosss@users.noreply.github.com>
2021-12-08 00:18:12 +0000

    Add files via upload
    motors support added

--- sservo.py
+++ sservo.py
@@ -1,13 +1,14 @@
 import RPi.GPIO as GPIO 
-from gpiozero import Servo
+
 from time import sleep
-flipperIn = 17
+from motors import *
+flipperIn = 21
+Ena = 22
 In1 = 27
-Ena = 16
-In2 = 22
-Enb = 21
-In4 = 26
-In3 = 20
+In2 = 17
+Enb = 19
+In4 = 6
+In3 = 13
 
 #Engine manager is manages two feeders and 1 servo(flipper).
 ###Today 30.11 the machines work with some help, what means you need to help feeder number 1 with roll it at start, and help servo
@@ -15,81 +16,76 @@ In3 = 20
 
 class EnginesManager:
 	def __init__(self):
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(In1, GPIO.OUT)
-		GPIO.setup(Ena, GPIO.OUT)
-		GPIO.setup(In2, GPIO.OUT)
-		GPIO.setup(In4, GPIO.OUT)
-		GPIO.setup(Enb, GPIO.OUT)
-		GPIO.setup(In3, GPIO.OUT)
-		self.p = GPIO.PWM(In2, 7000)
-		self.p2 = GPIO.PWM(Enb, 7000)
-		self.servo = Servo(flipperIn)
-		self._servoVal = 1
-		self.servo.value = self._servoVal
+		#self.servo = None
+		self.motor1 = None
+		self.motor2 = None
 	#engine num is the number of the machine , wooden is 1
-	def startEngine(self, EngineNum):
+	def startEngine(self, EngineNum, x=55, t=0):
 		print('starting engine num: ', EngineNum)
 		if EngineNum == 1:
-			GPIO.output(In1, GPIO.HIGH)
-			GPIO.output(Ena, GPIO.HIGH)
-			self.p.start(1)
+			#self.servo = Servo(flipperIn)
+			#self.servo.value = -1
+			self.motor1 = Motor(Ena, In1, In2, 1)
+			self.motor1.moveMotor(t=t, x=x)
 		elif EngineNum == 2:
-			GPIO.output(In3, GPIO.HIGH)
-			GPIO.output(In4, GPIO.HIGH)
-			self.p2.start(1)
+			#self.servo = Servo(flipperIn)
+			#self.servo.value = 1
+			self.motor2 = Motor(Enb, In4, In3, -1)
+			self.motor2.moveMotor(t=t, x=x)
    	#engine num is the number of the machine , wooden is 1
 	def stopEngine(self, EngineNum):
 		print('stopping engine num:', EngineNum)
 		if EngineNum == 1:
-			GPIO.output(In1, GPIO.LOW)
-			GPIO.output(Ena, GPIO.LOW)
-			self.p.stop()
+			if self.motor1 is not None:
+				#self.servo.value = 0
+				#self.servo.detach()
+				self.motor1.stopMotor()
 		elif EngineNum == 2:
-			GPIO.output(In3, GPIO.LOW)
-			GPIO.output(In4, GPIO.LOW)
-			self.p2.stop()
+			if self.motor2 is not None:
+				#self.servo.value = 0
+				#sleep(2)
+				#self.servo.detach()
+				self.motor2.stopMotor()
+
    #1 is straight(wire direction) and -1 is right, initilized to staright 
 	def Flip(self):
+		GPIO.setmode(GPIO.BCM)
+		GPIO.input(flipperIn)
+		self.servo = Servo(flipperIn)
 		if self._servoVal == 1:
 			self._servoVal = -1
 		else:
 			self._servoVal = 1
 		self.servo.value = self._servoVal
 		print('flipped! val: ', self._servoVal )
+		self.servo.detach()
+		GPIO.cleanup()
        
 
 def main():
 	print('started!!')
+	#GPIO.cleanup()
 	manager = EnginesManager()
 	#manager.Flip()
 	#sleep(2)
 	#manager.Flip()
 	#sleep(2)
 	#manager.Flip()
-	manager.startEngine(1)
-	sleep(5)
-	manager.stopEngine(1)
-	sleep(5)
-	manager.startEngine(2)
-	sleep(5)
+	manager.startEngine(2, x=65, t=5)
+	#sleep(2)
 	manager.stopEngine(2)
-	manager.Flip()
-	sleep(6)
-	manager.Flip()
-	sleep(6)
+	#sleep(5)
+	manager.startEngine(1, x=100, t=5)
+	manager.stopEngine(1)
+	#manager.Flip()
+	#sleep(2)
+	#manager.Flip()
+	#sleep(2)
+	
 
 
 
-#servo = Servo(17)
 
-#val = -1
-#while True:
-#	servo.value = val
-#	sleep(5)
-#	val = val+ 2
-#	if val > 1 :
-#		val = -1
 if __name__ == '__main__':
     	main()
 	
